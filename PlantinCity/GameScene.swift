c1d97011783016c796ffe75eeb36592310260ed6
Amrik <ztriplezero@hotmail.com>
2019-12-07 13:54:27 -0500

    Fetching real time Air Quality of City Delhi : Success!

--- PlantinCity/GameScene.swift
+++ PlantinCity/GameScene.swift
@@ -5,60 +5,37 @@
 //  Created by Z Angrazy Jatt on 2019-12-05.
 //  Copyright © 2019 Z Angrazy Jatt. All rights reserved.
 //
-
+import Foundation
 import SpriteKit
 import GameplayKit
-import Firebase
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
-class GameScene: SKScene {
+class GameScene: SKScene, CLLocationManagerDelegate {
     
     var airQualityLable : SKLabelNode?
     var dataAirQuality:String?
-    var ref = DatabaseReference()
     
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
      let API_KEY = "acfc24f10275cfffef7f40e6dd2e9b2ceca6f27a"
-    let apiURL = "https://api.waqi.info/map/bounds/?latlng=39.379436,116.091230,40.235643,116.784382&token=acfc24f10275cfffef7f40e6dd2e9b2ceca6f27a"
+    var lati = 28.7041
+    var longi = 77.1025
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+//let apiURL = "https://api.waqi.info/feed/geo:28.7041;77.1025/?token=acfc24f10275cfffef7f40e6dd2e9b2ceca6f27a"
     
     private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func sceneDidLoad() {
         self.airQualityLable = self.scene?.childNode(withName: "airqualityLabel") as? SKLabelNode
         print("Hello from game Scene")
-        ref = Database.database().reference()
-        //self.ref.child("airquality").childByAutoId().setValue(6623)
-        self.ref.child("airquality/airquality").setValue(6623000)
+    
         self.lastUpdateTime = 0
-        // adding Air Poluton Label
-        
-        ref = Database.database().reference().child("airquality")
-        ref.observeSingleEvent(of: .value, with: { snapshot in
-            if let snap = snapshot.value as? [String : AnyObject] {
-               if let result = snap["airquality"] as? String {
-                self.airQualityLable = self.scene?.childNode(withName: "airqualityLabel") as? SKLabelNode
-                //self.dataAirQuality = result
-                self.airQualityLable?.text = result
-                
-               }
-                else{
-                    self.airQualityLable?.text = "something went wrong"
-                }
-               }
-            
-            })
-//        self.airQualityLable = self.scene?.childNode(withName: "airqualityLabel") as? SKLabelNode
-        //self.airQualityLable?.text = self.dataAirQuality
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -72,7 +49,36 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.locationServicesEnabled()){
+                   locationManager.delegate = self
+                   locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                   locationManager.startUpdatingLocation()
+               }
     }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lati = location.coordinate.latitude
+        longi = location.coordinate.longitude
+        Alamofire.request("https://api.waqi.info/feed/geo:28.7041;77.1025/?token=acfc24f10275cfffef7f40e6dd2e9b2ceca6f27a").responseJSON {
+            response in
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonQuality = jsonResponse["data"]
+          self.airQualityLable?.text = jsonQuality["aqi"].stringValue
+               print("weather data printing")
+                
+               // let suffix = iconName.suffix(1)
+                
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           print(error.localizedDescription)
+       }
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -100,9 +106,6 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
